1d6aaf647340e23cf441a6b8adb88387b422a51b
Inioluwa Daniel <jocdev28@gmail.com>
2026-02-14 12:56:15 +0100

    Finished Project: Day 17

--- Day17/quiz-game-start/main.py
+++ Day17/quiz-game-start/main.py
@@ -1,15 +1,18 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
-# for question in question_data:
-#     question_bank.append(Question(question["text"], question["answer"]))
-#
-# print(question_bank)
-
 for index in range(len(question_data)):
     current_question = question_data[index]
-    question_bank.append([current_question["text"], current_question["answer"]])
+    question = Question(current_question["question"], current_question["correct_answer"])
+    question_bank.append(question)
 
-print(question_bank)
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz.")
+print(f"Your final score is: {quiz.score}/{quiz.question_number}")
