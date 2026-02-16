8f21f1c719d1c87c8b9e86d15de5f4758e1f473e
Inioluwa Gbemisola Daniel <Inioluwa.Daniel@ehealthafrica.local>
2026-02-16 16:52:43 +0100

    Initial commit with Day 18

---
+++ shapes.py
@@ -0,0 +1,20 @@
+from idlelib.colorizer import color_config
+from turtle import Turtle, Screen
+
+tim = Turtle()
+
+colors = ["crimson", "dark green", "wheat", "deep sky blue", "dark slate gray", "dark orchid", "magenta", "coral"]
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for index in range(3, 11):
+    tim.pencolor(colors[index-3])
+    draw_shape(index)
+
+
+screen = Screen()
+screen.exitonclick()
