8d7d50fd9efb10551a5f35b4285ef2ed79d20f80
Inioluwa Gbemisola Daniel <Inioluwa.Daniel@ehealthafrica.local>
2026-03-04 10:12:36 +0100

    Final commit for Day 20

--- Day20/snake.py
+++ Day20/snake.py
@@ -1,11 +1,16 @@
 from turtle import Turtle
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+LEFT = 180
+DOWN = 270
+RIGHT = 0
 
 class Snake:
     def __init__(self):
         self.snake = []
         self.create_snake()
+        self.head = self.snake[0]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -20,4 +25,20 @@ class Snake:
             new_x = self.snake[body - 1].xcor()
             new_y = self.snake[body - 1].ycor()
             self.snake[body].goto(new_x, new_y)
-        self.snake[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
